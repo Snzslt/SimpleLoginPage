119e6fcf0cae53e161d426ab57981695c1c77178
Sanaz Soltani <74157401+Snzslt@users.noreply.github.com>
2024-01-20 19:55:35 -0500

    Getting User Input Using Ref

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,26 +1,22 @@
 
-import {useState} from 'react';
+import {useRef} from 'react';
 
 
 export default function Login() {
 
-  const [enteredValues, setEnteredValues] = useState({
-    email:'',
-    password:''
-  });
+ const email = useRef();
+ const password = useRef();
 
   function handleSubmit(event){
  event.preventDefault();
+ const enteredEmail = email.current.value;
+ const enteredPassword = password.current.value;
+
+ console.log(enteredEmail, enteredPassword);
  
   }
 
-  function handleInputChange(identifier, value){
-   setEnteredValues(prevValues => ({
-     ...prevValues,
-     [identifier]: value
-
-   }));
-  }
+ 
 
 
   return (
@@ -34,8 +30,7 @@ export default function Login() {
            id="email"
            type="email"
            name="email"
-           onChange={(event) => handleInputChange('email', event.target.value)}
-           value={enteredValues.email}/>
+           ref={email}/>
         </div>
 
         <div className="control no-margin">
@@ -44,8 +39,7 @@ export default function Login() {
            id="password"
           type="password"
           name="password"
-          onChange={(event) => handleInputChange('password', event.target.value)}
-           value={enteredValues.password} />
+          ref = {password} />
         </div>
       </div>
 
